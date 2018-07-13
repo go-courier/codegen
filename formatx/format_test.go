c08ddd2dc3e619e9ab62b80ac1352b88665d0354
Morlay <morlay.null@gmail.com>
2018-07-13 14:18:52 +0800

    format go get vendor before testings

--- formatx/format_test.go
+++ formatx/format_test.go
@@ -2,17 +2,17 @@ package formatx
 
 import (
 	"os"
+	"os/exec"
 	"path"
 	"testing"
 
-	"fmt"
 	"github.com/stretchr/testify/require"
 )
 
 func TestProcess(t *testing.T) {
-	cwd, _ := os.Getwd()
-	fmt.Println(path.Join(cwd, "format2_test.go"))
+	exec.Command("go", "get", "github.com/davecgh/go-spew/spew").CombinedOutput()
 
+	cwd, _ := os.Getwd()
 	result, _ := Format(path.Join(cwd, "format2_test.go"), []byte( /* language=go */ `package formatx
 
 import (
